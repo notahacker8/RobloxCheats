d85f9680b5ee2709ebea9cd1255242a0483e21d7
King Doggo <77363964+notahacker8@users.noreply.github.com>
2022-09-30 19:35:48 -0700

    Add files via upload

---
+++ ESP/ESP.h
@@ -0,0 +1,12 @@
+//
+//  ESP.h
+//  ESP
+//
+//  Created by Dimitri Armendariz on 9/13/22.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <CoreGraphics/CoreGraphics.h>
+#include "pthread.h"
+#include <malloc/malloc.h>
