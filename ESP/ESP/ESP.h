e275253d2374708d6e4a2400f475d02c5d08ffa0
King Doggo <77363964+notahacker8@users.noreply.github.com>
2023-04-10 00:33:53 -0700

    Add files via upload

---
+++ ESP/ESP/ESP.h
@@ -0,0 +1,12 @@
+//
+//  ESP.h
+//  ESP
+//
+//  Created by me on 9/13/22.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <CoreGraphics/CoreGraphics.h>
+#include "pthread.h"
+#include <malloc/malloc.h>
