61935dc67578fdbc91761a8aa2f190c0cf128639
King Doggo <77363964+notahacker8@users.noreply.github.com>
2024-05-13 19:13:59 -0700

    Delete RobloxCheats directory

--- RobloxCheats/RobloxCheats/Objects/humanoid.h
+++
@@ -1,7 +0,0 @@
-
-
-char* rbx_humanoid_get_displayname(task_t task, vm_address_t humanoid, long* inout_len)
-{
-    return vm_read_rbx_string(task, humanoid + RBX_HUMANOID_DISPLAYNAME_OFFSET, inout_len);
-}
-
