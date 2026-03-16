c907d77b9d76c404e4ce4457cc7e48ebccb953f8
Atlastoby <77363964+notahacker8@users.noreply.github.com>
2026-03-16 01:42:54 -0700

    Delete RobloxCheats directory

--- RobloxCheats/RobloxCheats/Objects/tool.h
+++
@@ -1,9 +0,0 @@
-
-/*
-
-void rbx_tool_set_canbedropped(task_t task, vm_address_t tool, char new_value)
-{
-    vm_write(task, tool + RBX_TOOL_CANBEDROPPED_OFFSET, (vm_address_t)&new_value, sizeof(char));
-}
-
-*/
